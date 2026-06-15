bd088610d0d0d82e99b78f1dfb831ef8f4fab659
oggyforsure <oggyforsure@gmail.com>
2026-06-15 17:03:46 +0545

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -27,7 +27,7 @@ export const botConfig = {
         // Text users will see (example: "Playing /help | Titan Bot").
         name: "Eating cheese",
         // Activity type number (0 = Playing).
-        type: 4, Eating
+        type: 5,
       },
     ],
   },
